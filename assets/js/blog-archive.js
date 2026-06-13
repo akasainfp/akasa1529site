87d8d6dd80622738ea64d5fff41edd6c1496eda5
akasainfp <akasainfp@users.noreply.github.com>
2026-06-13 11:55:11 +0900

    Hide blog excerpts

--- assets/js/blog-archive.js
+++ assets/js/blog-archive.js
@@ -62,7 +62,6 @@ function renderPosts(posts) {
             eyecatch +
             '<div class="blog-meta"><span>' + (date || 'NO DATE') + '</span><span class="blog-category">' + escapeHtml(post.category) + '</span>' + tagText + '</div>' +
             '<h2>' + escapeHtml(post.title) + '</h2>' +
-            '<p class="blog-excerpt">' + escapeHtml(post.excerpt) + '</p>' +
             '<div class="blog-body">' + (post.body || '<p>' + escapeHtml(post.excerpt) + '</p>') + '</div>' +
             '<div class="blog-actions"><button class="blog-toggle" type="button" data-blog-toggle aria-expanded="false">もっと見る</button></div>' +
         '</article>';
